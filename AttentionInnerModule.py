d17426b6388f983ce1e145be2aef712f5698a94d
dangkh <dangkh109@gmail.com>
2022-12-26 16:20:50 +0700

    Update AttentionInnerModule.py

--- AttentionInnerModule.py
+++ AttentionInnerModule.py
@@ -1,15 +1,17 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import dgl
+import numpy as np 
+import dgl.function as fn
 
 class GATInnerLayer(nn.Module):
     def __init__(self, g, in_dim, out_dim):
-        super(GATLayer, self).__init__()
+        super(GATInnerLayer, self).__init__()
         self.g = g
         self.in_dim = in_dim
-        self.qMask = nn.Linear(in_dim, out_dim, bias=False)
-        self.kMask = nn.Linear(in_dim, out_dim, bias=False)
+        self.qMask = nn.Linear(in_dim, in_dim, bias=False)
+        self.kMask = nn.Linear(in_dim, in_dim, bias=False)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -17,45 +19,50 @@ class GATInnerLayer(nn.Module):
         gain = nn.init.calculate_gain('relu')
         nn.init.xavier_normal_(self.qMask.weight, gain=gain)
         nn.init.xavier_normal_(self.kMask.weight, gain=gain)
-        nn.init.xavier_normal_(self.attn_fc.weight, gain=gain)
 
     def edge_attention(self, edges):
         # edge UDF for equation (2)
-        z2 = torch.cat([edges.src['z'], edges.dst['z']], dim=1)
-        a = self.attn_fc(z2)
-        return {'e': F.leaky_relu(a)}
+        qVal = self.qMask(edges.src['h'])
+        qVal = torch.unsqueeze(qVal, 2)
+        kVal = self.kMask(edges.src['h'])
+        kVal = torch.unsqueeze(kVal, 1)
+        score = (qVal @ kVal)  / np.sqrt(self.in_dim)
+        score = F.softmax(score, dim=1)
+        origin = torch.unsqueeze(edges.src['h'], 2)
+        attVal = torch.bmm(score, origin).sum(dim = 2)
+        # print(attVal.shape)
+        return {'a': attVal, 's': score}
 
     def message_func(self, edges):
         # message UDF for equation (3) & (4)
-        return {'z': edges.src['z'], 'e': edges.data['e']}
+        return {'a': edges.data['a'], 's': edges.data['s']}
 
     def reduce_func(self, nodes):
-        # reduce UDF for equation (3) & (4)
-        # equation (3)
-        alpha = F.softmax(nodes.mailbox['e'], dim=1)
-        # equation (4)
-        h = torch.sum(alpha * nodes.mailbox['z'], dim=1)
-        return {'h': h}
+        # self.counter += 1
+        # print(f"debug counter at: {self.counter}")
+        # print(nodes.mailbox)
+        # datas =nodes.mailbox['a'] 
+
+        # print(datas.shape)
+        # print(score.shape)
+        # lenFeature = datas.shape[-1]
+        # datas = datas.reshape(-1, lenFeature)
+        # print(datas.shape)
+        out = torch.mean(nodes.mailbox['a'] , dim = 1)
+        # print(out.shape)
+        return {'h': out}
 
     def forward(self, h):
-        # equation (1)
-        qVal = self.qMask(h)
-        kVal = self.kMask(h)
-        score = torch.mm(qVal, kVal.T)  / torch.sqrt(self.in_dim)
-        score = F.softmax(score, dim = 1)
-        output = torch.mm(score, h)
-
-        self.g.ndata['z'] = z
-        # equation (2)
+        self.g.ndata['h'] = h
         self.g.apply_edges(self.edge_attention)
-        # equation (3) & (4)
+
         self.g.update_all(self.message_func, self.reduce_func)
         return self.g.ndata.pop('h')
 
 
 class MultiHeadGATInnerLayer(nn.Module):
     def __init__(self, g, in_dim, out_dim, num_heads, merge='cat'):
-        super(MultiHeadGATLayer, self).__init__()
+        super(MultiHeadGATInnerLayer, self).__init__()
         self.heads = nn.ModuleList()
         for i in range(num_heads):
             self.heads.append(GATInnerLayer(g, in_dim, out_dim))
@@ -69,3 +76,12 @@ class MultiHeadGATInnerLayer(nn.Module):
         else:
             # merge using average
             return torch.mean(torch.stack(head_outs))
+
+
+g = dgl.graph(([0,1,2,3,2,5], [1,2,3,4,0,3]))
+g = dgl.add_self_loop(g)
+feat = torch.rand(6, 10)
+gatconv = MultiHeadGATInnerLayer(g, 10, 2, num_heads=3)
+res = gatconv(feat)
+# print(res.shape)
+# print(res)
